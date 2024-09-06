e71de672cf98c166a0a42043cfa4da6809589b82
Paul <teaguep@hotmail.com>
2024-09-06 11:39:18 +0100

    changed handleGrossPriceChange to 2 decimal places

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ function App() {
         <VatRateField customstyle="field" vatRateChanged={handleVatRateChanged} value={vatRate} updatePrices={updatePrices} />
         <PriceEntryField customstyle="field" label="Price excl VAT: " priceChanged={handleNetPriceChange} price={netPrice === 0.0 ? "" : netPrice} />
         <DisplayBlock customstyle="field" label="VAT to pay: " value={vatToPay.toFixed(2)} />
-        <PriceEntryField customstyle="field" label="Price incl VAT: " priceChanged={handleGrossPriceChange} price={grossPrice === 0.0 ? "" : grossPrice} />
+        <PriceEntryField customstyle="field" label="Price incl VAT: " priceChanged={handleGrossPriceChange.toFixed(2)} price={grossPrice === 0.0 ? "" : grossPrice} />
       </div>
     </div>
   );
